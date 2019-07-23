25f60f7848628f34ac1844f71ed9966247c586e1
codemonk-19 <40389209+codemonk-19@users.noreply.github.com>
2019-07-23 10:00:04 +0530

    Create simple_so.cpp

---
+++ simple_so.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+int a,b;
+cout<<"\n Enter two numbers ";
+cin>>a>>b;
+cout<<"\n the addition of two will give "<<a+b;
+cout<<"\n the substraction of two will give "<<abs(a-b);
+cout<<"\n the multiplication of two will give "<<a*b;
+cout<<"\n the division of two will give "<<a/b;
+
+return 0;
+}
